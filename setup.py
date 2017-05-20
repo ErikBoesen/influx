6a676ed294e60e36d30fb4d905ab614344f51405
ErikBoesen <me@erikboesen.com>
2017-05-20 11:33:46 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='influx',
+      version='0.0.1',
+      description='A very simple web server based off http.server.',
+      url='https://github.com/ErikBoesen/influx',
+      author='Erik Boesen',
+      author_email='me@erikboesen.com',
+      license='MIT',
+      packages=['influx'],
+      install_requires=[],
+      zip_safe=False)
